fa535728e72a19a64ba8fe023f1cc45f0c7187f6
gioperalto <gyoperalto@gmail.com>
2017-02-06 06:54:53 -0500

    Implemented rosters

--- config/api-endpoints.js
+++ config/api-endpoints.js
@@ -3,6 +3,8 @@
 var SeasonController = require('../app/controllers/SeasonController');
 var ChampionController = require('../app/controllers/ChampionController');
 var PlayerController = require('../app/controllers/PlayerController');
+var MoodController = require('../app/controllers/MoodController');
+var RosterController = require('../app/controllers/RosterController');
 
 module.exports = function(app) {
 
@@ -67,13 +69,43 @@ module.exports = function(app) {
     });
   });  
 
-  app.get('/api/season/create/:name', function(req, res) {
-    var name = req.params.name;
+  app.get('/api/season/create/:number', function(req, res) {
+    var number = req.params.number;
     
-    SeasonController.createSeason(name, function(season, err) {
+    SeasonController.createSeason(number, function(season, err) {
       res.status(200).json(season);
     });
   });
 
-  // TODO: Active roster
+  // =====================================
+  // SEASONS =============================
+  // =====================================
+
+  app.get('/api/moods', function(req, res) {
+    MoodController.getMoods(function(moods, err) {
+      res.status(200).json(moods);
+    });
+  }); 
+
+  app.get('/api/moods/random', function(req, res) {
+    MoodController.getRandomMood(function(moods, err) {
+      res.status(200).json(moods);
+    });
+  }); 
+
+  // =====================================
+  // ROSTERS =============================
+  // =====================================
+
+  app.get('/api/rosters/active', function(req, res) {
+    RosterController.getActiveRoster(function(roster, err) {
+      res.status(200).json(roster);
+    });
+  });
+
+  app.get('/api/rosters/generate', function(req, res) {
+    RosterController.generate(function(roster, err) {
+      res.status(200).json(roster);
+    });
+  });
 };
